9d918caa36e8561307ddec2c900d29f1afb26f11
GM-11 <gmathur1001@gmail.com>
2023-01-21 16:59:22 +0530

    added hover to login button

--- components/header.js
+++ components/header.js
@@ -5,10 +5,10 @@ function Header() {
     <div className="bg-background text-white  rounded-3xl m-5 flex flex-row justify-between">
       <div className="px-6 py-4">ADILO</div>
       <ul className="flex  m-3  flex-row">
-        <button className="px-3 py-1">Home</button>
-        <button className="px-3 py-1">About</button>
-        <button className="px-3 py-1">Contact Us</button>
-        <button className="mx-3 text-background px-3 py-1 bg-white rounded-3xl">
+        <button className="mx-5 py-1">Home</button>
+        <button className="mx-5 py-1">About</button>
+        <button className="mx-5 py-1">Contact Us</button>
+        <button className="mx-5 text-background py-1 px-3 bg-white rounded-3xl border-2 border-white hover:bg-transparent hover:text-white  ease-in duration-100">
           Login
         </button>
       </ul>
